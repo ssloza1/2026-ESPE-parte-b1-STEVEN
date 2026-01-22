11f3b03a1eddb5499803d6ff2d0c8a2fa1b8b349
Steven <ssloza1@espe.edu.ec>
2026-01-22 10:29:49 -0500

    update-Experiment-b1

--- src/main/java/es/upm/grise/profundizacion/exceptions/ExistingUserException.java
+++ src/main/java/es/upm/grise/profundizacion/exceptions/ExistingUserException.java
@@ -2,9 +2,7 @@ package es.upm.grise.profundizacion.exceptions;
 
 public class ExistingUserException extends Exception {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 }
